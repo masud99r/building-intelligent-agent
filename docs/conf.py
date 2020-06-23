970f41caf220c2f92e93bfc61684106fb2c70c46
masud rahman <masud99rh@gmail.com>
2020-06-23 15:04:14 -0400

    update

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'recommonmark',
     'sphinx_rtd_theme',
+    'sphinx_materialdesign_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
