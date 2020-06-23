95987dd0a91fe2dde918a4112d26938a042177cc
masud rahman <masud99rh@gmail.com>
2020-06-23 15:16:17 -0400

    update

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = 'sphinx_materialdesign_theme' # sphinx_materialdesign_theme, sphinx_rtd_theme, alabaster
-
+html_theme_path = ["_themes", ]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
